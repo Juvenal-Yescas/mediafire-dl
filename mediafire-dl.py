bc7b77b37720e12719b5bb7dcb93531d89998f4e
Juvenal Yescas <juvenal@mail.com>
2019-06-06 18:28:43 -0500

    Create mediafire-dl.py

---
+++ mediafire-dl.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import os.path as osp
+import re
+import shutil
+import sys
+import tempfile
+import requests
+import six
+import tqdm
+
+CHUNK_SIZE = 512 * 1024  # 512KB
+
+def extractDownloadLink(contents):
+    for line in contents.splitlines():
+        m = re.search(r'href="(https://download[^"]+)', line)
+        if m:
+            return m.groups()[0]
+
+def download(url, output, quiet):
+    url_origin = url
+    sess = requests.session()
+
+    while True:
+        res = sess.get(url, stream=True)
+        if 'Content-Disposition' in res.headers:
+            # This is the file
+            break
+
+        # Need to redirect with confiramtion
+        url = extractDownloadLink(res.text)
+
+        if url is None:
+            print('Permission denied: %s' % url_origin, file=sys.stderr)
+            print(
+                "Maybe you need to change permission over "
+                "'Anyone with the link'?",
+                file=sys.stderr,
+            )
+            return
+
+    if output is None:
+        m = re.search(
+            'filename="(.*)"', res.headers['Content-Disposition']
+        )
+        output = m.groups()[0]
+        # output = osp.basename(url)
+
+    output_is_path = isinstance(output, six.string_types)
+
+    if not quiet:
+        print('Downloading...', file=sys.stderr)
+        print('From:', url_origin, file=sys.stderr)
+        print(
+            'To:',
+            osp.abspath(output) if output_is_path else output,
+            file=sys.stderr,
+        )
+
+    if output_is_path:
+        tmp_file = tempfile.mktemp(
+            suffix=tempfile.template,
+            prefix=osp.basename(output),
+            dir=osp.dirname(output),
+        )
+        f = open(tmp_file, 'wb')
+    else:
+        tmp_file = None
+        f = output
+
+    try:
+        total = res.headers.get('Content-Length')
+        if total is not None:
+            total = int(total)
+        if not quiet:
+            pbar = tqdm.tqdm(total=total, unit='B', unit_scale=True)
+        for chunk in res.iter_content(chunk_size=CHUNK_SIZE):
+            f.write(chunk)
+            if not quiet:
+                pbar.update(len(chunk))
+        if not quiet:
+            pbar.close()
+        if tmp_file:
+            f.close()
+            shutil.move(tmp_file, output)
+    except IOError as e:
+        print(e, file=sys.stderr)
+        return
+    finally:
+        try:
+            if tmp_file:
+                os.remove(tmp_file)
+        except OSError:
+            pass
+    return output
+
+if __name__ == "__main__":
+    for url in sys.argv[1:]:
+        download(url,output=None,quiet=None)
